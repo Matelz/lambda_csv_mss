9f967316818019554fabb51b3f9fb68d4cc99c20
Matelz <49626198+Matelz@users.noreply.github.com>
2025-08-15 17:24:56 -0300

    ci/cd: add deployment workflow

---
+++ db/s3.go
@@ -0,0 +1,36 @@
+package db
+
+import (
+	"context"
+	"errors"
+	"log"
+
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go-v2/service/s3/types"
+)
+
+type S3BucketBasics struct {
+	S3Client *s3.Client
+}
+
+func NewS3BucketBasics(client *s3.Client) S3BucketBasics {
+	return S3BucketBasics{
+		S3Client: client,
+	}
+}
+
+func (basics S3BucketBasics) DownloadFile(ctx context.Context, object *s3.GetObjectInput) (*s3.GetObjectOutput, error) {
+	result, err := basics.S3Client.GetObject(ctx, object)
+	if err != nil {
+		var noKey *types.NoSuchKey
+		if errors.As(err, &noKey) {
+			log.Printf("Can't get object %s from bucket %s. No such key exists.\n", object.Key, object.Bucket)
+			err = noKey
+		} else {
+			log.Printf("Couldn't get object %v:%v. Here's why: %v\n", object.Bucket, object.Key, err)
+		}
+		return nil, err
+	}
+
+	return result, err
+}
